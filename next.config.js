cc1c8997550b23f8db088c455a11330341b9d9e9
Ivan Bosnic <ivan.bosnic.st@gmail.com>
2025-08-07 16:25:33 +0200

    Removed github pages suffix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  // Fix asset paths for GitHub Pages deployment
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/e-invoice-hr/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/e-invoice-hr' : '',
 }
 
 module.exports = nextConfig 
